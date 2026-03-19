b7c97f5b057362255a28fe88496a9fc1e102f55d
mithikhobragade3110-star <mithikhobragade3110@gmail.com>
2026-03-19 20:50:50 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+name = input("What is your name? " "Mithilesh")
+age = input("How old are you? " "23")
+hobby = input("What is your favourite hobby? " "Cricket")
+city = input("Where do you live? " "Nagpur")
+
+print(" ")
+print("Welcome Mithilesh! ")
+print("You are 23 years old and love Cricket. ")
+print("I live in Nagpur. ")
+print("We are happy to have you here! ")
+
